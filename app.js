a647f859e6768feef9578612dafb120b6fc88271
arameshraju <alluri.ramesh@gmail.com>
2016-10-08 11:58:10 +0530

    service and masters put and gets

--- app.js
+++ app.js
@@ -11,11 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Routes 
 var dept = require('./routes/departments');
+var emp = require('./routes/employee');
+var food = require('./routes/fooditem');
+var sg = require('./routes/stockgroup');
+var si = require('./routes/stockitem');
+var store = require('./routes/store');
+var token = require('./routes/token');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected! and enabled Rest
     console.log("Data base connect");
     app.use('/api/department', dept);
+    app.use('/api/employee', emp);
+    app.use('/api/fooditem', food);
+    app.use('/api/stockgroup', sg);
+    app.use('/api/stockitem', si);
+    app.use('/api/store', store);
+    app.use('/api/token', token);
 
 });
 
